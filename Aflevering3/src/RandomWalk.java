8e1f7998bf2414dd328069deffa558054847c0db
MrJuses <83298438+MrJuses@users.noreply.github.com>
2022-03-30 15:07:55 +0200

    Update RandomWalk.java

--- Aflevering3/src/RandomWalk.java
+++ Aflevering3/src/RandomWalk.java
@@ -1,2 +1,47 @@
+import java.util.*;
+
+import lib.StdDraw;
+
 public class RandomWalk {
+    public static void main(String[] args){
+        randomWalk();
+    }
+
+    public static void randomWalk() {
+        Random rand = new Random();
+        int[] oldCoords = {0,0};
+        int[] newCoords = {0,0};
+        int steps = 0;
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter size of grid:");
+        int n = console.nextInt();
+
+        StdDraw.setXscale(-n, n);
+        StdDraw.setYscale(-n, n);
+        StdDraw.setPenRadius(5.0/1000);
+
+        while(Math.abs(newCoords[0]) <= n && Math.abs(newCoords[1]) <= n){
+            int Direction = rand.nextInt(4);
+            if(Direction == 0){
+                newCoords[0]++;
+            } if(Direction == 1){
+                newCoords[1]++;
+            } if(Direction == 2){
+                newCoords[0]--;
+            } if(Direction == 3) {
+                newCoords[1]--;
+            }
+
+            StdDraw.show(0);
+            StdDraw.line(oldCoords[0],oldCoords[1],newCoords[0],newCoords[1]);
+
+            oldCoords[0] = newCoords[0];
+            oldCoords[1] = newCoords[1];
+            steps++;
+            System.out.println("Position = (" + oldCoords[0] + "," + oldCoords[1] + ")");
+        }
+        System.out.println("\nTotal number of steps = " + steps);
+    }
 }
+
