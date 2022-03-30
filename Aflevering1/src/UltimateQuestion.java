6849025c70ec60ce28a7b1a77f33c657501da4bf
MrJuses <83298438+MrJuses@users.noreply.github.com>
2022-03-30 14:53:54 +0200

    Update UltimateQuestion.java

--- Aflevering1/src/UltimateQuestion.java
+++ Aflevering1/src/UltimateQuestion.java
@@ -1,2 +1,10 @@
 public class UltimateQuestion {
+    public static void main(String[] args) {
+
+        // Declare variables
+        double x = 7.2;
+
+        // Compute and print. We use Math.round to round from 42.0 to 42
+        System.out.println(Math.round(1 + 3 + x + x + x + x + x + 5 / 4 * 2));
+    }
 }
