732bbc7613b8240f67cc9930ceb2d745132ac53c
MrJuses <83298438+MrJuses@users.noreply.github.com>
2022-03-30 14:53:20 +0200

    Update Remember.java

--- Aflevering1/src/Remember.java
+++ Aflevering1/src/Remember.java
@@ -1,2 +1,11 @@
-public class Remember {
+public class Remember{
+    //We create a repeat method so we can call it later to avoid redundancy
+    public static void repeat() {
+        System.out.println("Use \"\\\\\" to obtain a ’backslash’ character.");
+    }
+    public static void main(String[] args){
+        repeat();
+        System.out.println("Remember:");
+        repeat();
+    }
 }
