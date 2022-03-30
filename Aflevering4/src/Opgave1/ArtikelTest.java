d5f11f83f5e0e0dedd81f309ce9d742fb3574850
MrJuses <83298438+MrJuses@users.noreply.github.com>
2022-03-30 16:55:02 +0200

    Update ArtikelTest.java

--- Aflevering4/src/Opgave1/ArtikelTest.java
+++ Aflevering4/src/Opgave1/ArtikelTest.java
@@ -1,4 +1,35 @@
 package Opgave1;
 
+import java.util.*;
+
 public class ArtikelTest {
+    public static void main(String[] args){
+        Forlag forlag = new Forlag();
+        Tidsskrift tidsskrift = new Tidsskrift();
+        Artikel artikel = new Artikel();
+
+        forlag.add("University Press","Denmark");
+
+        Tidsskrift.newTidsskrift("Journal of Logic");
+        Tidsskrift.newTidsskrift("Journal of Brain");
+        Tidsskrift.setForlag(0,"University Press");
+        Tidsskrift.setIssn(0,"Unknown");
+        Tidsskrift.setForlag(1,"University Press");
+        Tidsskrift.setIssn(1,"Unknown");
+
+        Artikel.CreateArticle("A. Abe & A. Turing","A","Journal of Logic");
+        Artikel.CreateArticle("B. Bim","B","Journal of Logic");
+        int[] B = {1};
+
+
+        System.out.println(Arrays.toString(Forlag.listforlag.get(0)));
+        System.out.println(Arrays.toString(Tidsskrift.listtidsskrift.get(0)));
+        System.out.println(Arrays.toString(Tidsskrift.listtidsskrift.get(1)));
+        System.out.println(Arrays.toString(Artikel.listarticle.get(0)));
+        System.out.println(Arrays.toString(Artikel.listarticle.get(1)));
+        forlag.showList();
+        artikel.setRefList(0,B);
+        Artikel.printRefList(0);
+
+    }
 }
