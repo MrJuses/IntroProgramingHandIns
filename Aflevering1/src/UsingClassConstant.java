5b73ffa6c917db33f7243036c48a16b8e55c3323
MrJuses <83298438+MrJuses@users.noreply.github.com>
2022-03-30 14:54:29 +0200

    Update UsingClassConstant.java

--- Aflevering1/src/UsingClassConstant.java
+++ Aflevering1/src/UsingClassConstant.java
@@ -1,2 +1,14 @@
 public class UsingClassConstant {
+    public static final int LINES = 3;
+    public static void main(String[] args) {
+        for (int i = 1; i <= LINES ; i++) {
+            for (int j = 1; j <= LINES; j++) {
+                System.out.print("+-");
+            }
+            //We move the print function one level in
+            // so that it doesn't execute at the end of the program
+            // but at the end of each line
+            System.out.println("+");
+        }
+    }
 }
