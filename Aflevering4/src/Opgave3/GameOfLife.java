94463d42866b89446f23c6415e5222920ad60a17
MrJuses <emilmhp@gmail.com>
2022-03-31 09:59:30 +0200

    game of life

--- Aflevering4/src/Opgave3/GameOfLife.java
+++ Aflevering4/src/Opgave3/GameOfLife.java
@@ -5,14 +5,19 @@ import java.util.*;
 
 public class GameOfLife {
     public int[][] grid;
+    public int[][] futuregrid;
     int alive;
     Random rand = new Random();
 
     public static void main(String[] args){
-        GameOfLife gameOfLife = new GameOfLife();
-        gameOfLife.gameoflife(3);
-        StdDraw.point(1,1);
-        StdDraw.show();
+        while(true){
+            GameOfLife gameOfLife = new GameOfLife();
+            gameOfLife.gameoflife(3);
+            StdDraw.point(1,1);
+            StdDraw.show();
+            gameOfLife.state(3);
+        }
+
 
     }
     public void gameoflife(int n){
@@ -24,8 +29,8 @@ public class GameOfLife {
         grid =  new int [n][n];
         for(int x = 0 ; x < n ; x++){
             for(int y = 0 ; y < n ; y++){
-                grid[y][x] = rand.nextInt(0,2);
-                System.out.println(grid[y][x]);
+                grid[x][y] = rand.nextInt(0,2);
+                System.out.println(grid[x][y]);
             }
         }
         init(n);
@@ -33,8 +38,8 @@ public class GameOfLife {
     public void init(int n){
         for(int x = 0 ; x < n ; x++){
             for(int y = 0 ; y < n ; y++){
-                if(grid[y][x] == 1){
-                    StdDraw.point(y,x);
+                if(grid[x][y] == 1){
+                    StdDraw.point(x,y);
                     StdDraw.show();
                 }
             }
@@ -42,41 +47,68 @@ public class GameOfLife {
     }
     public void state(int n){
         //lonely
-        int spot = 0;
         for(int x = 0 ; x < n ; x++){
             for(int y = 0 ; y < n ; y++){
                 //Corners:
                 if(x == 0 && y == 0){
+                    update(pass(neighborcheck(x,1,y,1),x,y),x,y);
 
                 } else if(x == 0 && y == n-1){
+                    update(pass(neighborcheck(x,1,n-2,n-1),x,y),x,y);
 
                 } else if(x == n-1 && y == 0){
+                    update(pass(neighborcheck(0,1,n-2,n-1),x,y),x,y);
 
                 } else if(x == n-1 && y == n-1){
+                    update(pass(neighborcheck(0,1,n-2,n-1),x,y),x,y);
 
                 }
                 //Edges
                 else if(x == 0){
-
+                    update(pass(neighborcheck(0,1,1,1),x,y),x,y);
                 } else if(y == 0){
-
+                    update(pass(neighborcheck(0,1,0,1),x,y),x,y);
                 } else if(x == n-1){
-
+                    update(pass(neighborcheck(0,1,0,1),x,y),x,y);
                 } else if(y == n-1){
-
+                    update(pass(neighborcheck(0,1,0,1),x,y),x,y);
                 }
                 //Middle
                 else{
-
+                    update(pass(neighborcheck(0,1,0,1),x,y),x,y);
                 }
             }
         }
+
+        System.out.println(grid);
+        grid = futuregrid;
+        System.out.println(grid);
     }
-    public void neighborcheck(int start, int end){
-        for(int x = start ; x < end ; x++){
-            for(int y = start ; y < end ; y++){
-                this.alive =+ grid[x][y];
+    public int neighborcheck(int xs, int xe, int ys, int ye){
+        for(int x = xs ; x < xe ; x++){
+            for(int y = ys ; y < ye ; y++){
+                this.alive += grid[x][y];
             }
+        }
+        return this.alive;
+    }
+    public Boolean pass(int neighborcheck, int x, int y) {
+        int neighbors = neighborcheck-grid[x][y];
+        if(grid[x][y] == 1 && neighbors < 2){
+            return false;
+        } else if(grid[x][y] == 1 && neighbors > 3){
+            return false;
+        } else if(grid[x][y] == 1 && neighbors == 2 || neighbors == 3){
+            return true;
+        } else {
+            return true;
+        }
+    }
+    public void update(boolean status, int x, int y){
+        if(status){
+            futuregrid[x][y] = 1;
+        } else{
+            futuregrid[x][y] = 0;
         }
     }
 }
