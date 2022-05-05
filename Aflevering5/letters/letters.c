d6bb149b9b676bb783a4b3c50278b8bdf98b4072
MrJuses <emilmhp@gmail.com>
2022-05-05 14:07:58 +0200

    project done

--- Aflevering5/letters/letters.c
+++ Aflevering5/letters/letters.c
@@ -1,11 +1,28 @@
-// letters.c
-
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(int argc, char * argv[]) {
+int letters(char *s);
 
-// ...
+int main(){
+    // Bygger en string der kan have op til længden 10.000
+    char str[10000];
+    printf("> ");
+    // scanner input som string, der først bliver stoppet når den bliver mødt ved en ny linje (Enter knap)
+    scanf("%[^\n]", str);
+    // gemmer antallet af bogstaver i en integer kaldet result der så printes.
+    int result = letters(str);
+    printf("%d\n", result);
+    return 0;
+}
 
-  return 0;
+int letters(char *str){
+    // tager længden af strengen.
+    int length = strlen(str);
+    int count =0;
+    // looper igennem hele strengen og hvis den møder en karakter i alfabetet (isalpha) så tæller den counter 1 op.
+    for (int i = 0; i <length; i ++)
+        if (isalpha(str[i])) count++;
+    // count bliver returnet ind i result i main.
+    return count;
 }
